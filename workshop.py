6dc00b83da9d57da1ead3f70331e4a3d747b37b6
mukunddp <mukundparve98@gmail.com>
2023-05-07 16:32:29 +0530

    Workshop

--- workshop.py
+++ workshop.py
@@ -1,4 +1,77 @@
-a = 10
-b = 20
-c = a + b
-print(c)
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# name = 'Mukunda'
+# institute = "TechAmplifiers"
+
+# print(id(institute))
+
+
+# name = 'Mukund'
+# print(id(name))
+#
+# name = 'Mukunda'
+# print(id(name))
+
+# name_1 = 'Mukund'
+# print(id(name_1))
+# name_1 = name_1 + 'Parve'
+# print(id(name_1))
+
+# int -  Number
+# String - "Alphabates"
+# Float - 10.50
+# Boolean - True/ False
+# Tuple - (10, 15, 'Mukund', 10.50)
+
+# Mutable
+# list - [10, 'Mukund', 15.50, True]
+# l = [10, 'Mukund', 15.50, True]
+# print("List L ID :", id(l))
+# l.append(20)
+# print(l)
+# print("List updated L ID :", id(l))
+
+# set - {10, 'Mukund', 15.50, True}
+# s = {10, 'Mukund', 15.50, True}
+# print("Set S ID :", id(s))
+# s.add('Set')
+# print(s)
+# print("Set updated S ID :", id(s))
+
+# Dict -
+# d = {'Key': 'Value',
+#      'Key1': 'Value2'}
+# print(d)
+# print("Dict D ID :", id(d))  # id - same
+# print(d['Key1'])
+#
+# print('------------------------')
+#
+# d['Key1'] = 'value1'
+# print(d['Key1'])
+# print(d)
+# print("Dict D ID :", id(d))   # id - same
+
+# String Methods
+
+# name = "mukund parve"
+# # name.title()
+# print(name.title())
+# print(name.capitalize())
+# print(name.upper())
+
+
+# Slicing
+# sliced_string = name[0:8]
+# print(sliced_string)
+
+# Task for tomorrow :
+# email = input('Write email address : ')
+
+# username = email[0:email.index('@')]
+# print(username)
+# domain = email[email.index('@')+1:]
+# print(domain)
---
+++ workshop2.py
@@ -0,0 +1,64 @@
+# Functions
+# print('This is print Function', 'of\n My laptop', end='\n')
+# print('Next line')
+
+# functions - Organised and reusable code
+# def function_name(name, age):  # parameters
+#     print('My name is', name, 'and my age is', age)
+#
+#
+# # invoking / calling
+# function_name(age=24, name='Mukund Parve')  # arguments  Yes(1) No(3) NOTA(1)
+# function_name(age=22, name='Saniya Shaikh')
+# function_name(age=25, name='Suraj Kamble')
+
+
+# Object Oriented Programming
+
+# classes
+# Object
+# self
+# constructor
+
+
+# Features of OOPs
+
+# Inheritance - Inheriting properties of different  classes
+# Polymorphism - Many forms : method overloading & method overriding
+# Abstraction - Hiding actual implementation (for better understanding)
+# Encapsulation - showing only necessary data access modifiers : Private, Public, Protected
+
+# class ClassName:
+#     name = 'Inside Class'
+#
+#     def __init__(self, name):
+#         print('I am a Constructor' , name)
+#
+#     def fun(self):
+#         print(self.name)
+#         print("I am fun function of this class")
+
+
+# """Object""" Instance
+# . - accessing and modifing data
+# obj = ClassName("Tech Amplifiers")
+# # obj.var = 'Inside the object of class.'
+# # print(obj.var)  # accessing and modifing data
+#
+# saniya = ClassName('Mukund')
+# print(saniya.name)0
+
+# for Loop - Specific number of iterations
+# l = ['Mukund', 'Rajlaxmi', 'Sanket', 'Anjali', 'Saniya']
+# for i in l:
+#     print(i)
+#
+# a = 10
+# # while Loop - Don't know specific number of iterations
+# while a < 10:
+#     print(' I am learning Python')
+
+#
+
+# Pywhatkit
+#
